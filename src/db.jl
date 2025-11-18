4959c6411f4a91d2075048332859e6e112a070e2
Sebastian Mantilla Manzano <j.s.c.zuluaga@gmail.com>
2025-11-17 22:21:55 -0500

    cambios config DB

--- src/db.jl
+++ src/db.jl
@@ -1,16 +1,19 @@
 module DB
+
 using MySQL
 using TOML
 
-DB_CONFIG = TOML.parsefile("config.toml")["database"]
+DB_CONFIG = TOML.parsefile("/root/BaseDeDatosConcesionario/config.toml")["database"]
+
+export get_conn
 
 function get_conn()
-  return MySQL.connect(
-    host = DB_CONFIG[:host],
-    user = DB_CONFIG[:user],
-    password = DB_CONFIG[:password],
-    db = DB_CONFIG[:db]
-  )
+    return MySQL.connect(
+        host = DB_CONFIG["host"],
+        user = DB_CONFIG["user"],
+        password = DB_CONFIG["password"],
+        db = DB_CONFIG["db"]
+    )
 end
 
-end
+end 
